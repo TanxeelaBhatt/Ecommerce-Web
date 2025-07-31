ce6a30352e4ebd313fcab4a6929349efd400fa92
TanxeelaBhatt <tanzeelabhatt16@gmail.com>
2025-07-31 23:45:23 +0530

    updating colors

--- Ecommerce-Web.js
+++ Ecommerce-Web.js
@@ -1,16 +1,53 @@
 
-  const popup = document.getElementById("popup");
+//   const popup = document.getElementById("popup");
 
-  document.addEventListener("mousemove", function(event) {
-    popup.style.left = event.pageX + "px";
-    popup.style.top = event.pageY + "px";
-    popup.style.display = "block";
-  });
+//   document.addEventListener("mousemove", function(event) {
+//     popup.style.left = event.pageX + "px";
+//     popup.style.top = event.pageY + "px";
+//     popup.style.display = "block";
+//   });
 
-  document.addEventListener("mouseleave", function() {
-    popup.style.display = "none";
-  });
+//   document.addEventListener("mouseleave", function() {
+//     popup.style.display = "none";
+//   });
 
-  function moveRight() {
-    document.getElementById("scroll-container").scrollLeft += 100;
+//   function moveRight() {
+//     document.getElementById("scroll-container").scrollLeft += 100;
+// }
+// let tim=10;
+// var jim=9;
+
+// function SQ (num){
+//     return num*num;
+// }
+// console.log(SQ (10));
+// function greet (name){
+//     return 'hello ' + name;
+// }
+// console.log(greet ('tanxeela'));
+
+// const func = function (abc){
+//     return 'hello ' + abc;
+// }
+// console.log(func("bobby"));
+// abc="bob";
+// if(abc=="bobby"){
+//     console.log(("bobby is true"))
+// }
+// else{
+//     console.log(("bobby is not"))
+// }
+temp=34;
+
+if (temp>=43){
+console.log(("temperature is high ,its hot outside"));
 }
+elseif(temp<=40)
+    console.log(("temperature is moderate"));
+
+elseif(temp<35)
+    console.log(("its cold outside"));
+
+// else(){
+//     console.log(("no weather updates"));
+// }
